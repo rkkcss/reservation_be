44581cc2fd3e3f6f08e13df76a2eddcc24d6dbee
rkkcss <61686634+rkkcss@users.noreply.github.com>
2026-01-16 19:23:30 +0100

    fix: prod quartz trying 1.4

--- src/main/java/hu/daniinc/reservation/config/QuartzConfig.java
+++ src/main/java/hu/daniinc/reservation/config/QuartzConfig.java
@@ -1,6 +1,7 @@
 package hu.daniinc.reservation.config;
 
 import hu.daniinc.reservation.service.jobs.AutowiringSpringBeanJobFactory;
+import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -24,13 +25,16 @@ public class QuartzConfig {
     public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer() {
         return schedulerFactoryBean -> {
             schedulerFactoryBean.setJobFactory(jobFactory);
-            // EZ A SOR FOGJA KÉNYSZERÍTENI A JDBC-RE:
             schedulerFactoryBean.setDataSource(dataSource);
             schedulerFactoryBean.setTransactionManager(transactionManager);
-
             schedulerFactoryBean.setOverwriteExistingJobs(true);
             schedulerFactoryBean.setWaitForJobsToCompleteOnShutdown(true);
             schedulerFactoryBean.setAutoStartup(true);
+
+            // Quartz properties
+            Properties quartzProperties = new Properties();
+            quartzProperties.put("org.quartz.jobStore.dataSource", "quartzDS");
+            schedulerFactoryBean.setQuartzProperties(quartzProperties);
         };
     }
 }
